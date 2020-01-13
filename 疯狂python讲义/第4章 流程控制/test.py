7d21688b4c4ef2011d4df2363a7ef8d88aeefccc
yangshuaifeng1987 <490055850@qq.com>
2020-01-13 23:48:29 +0800

    new file

---
+++ 疯狂python讲义/第4章 流程控制/test.py
@@ -0,0 +1,11 @@
+print("九九乘法表")
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('{}x{}={}\t'.format(j, i, i * j), end="")
+    print()
+
+print("循环输出等腰三角形")
+
+for i in range(4):
+    for j in range(4):
+        pass
