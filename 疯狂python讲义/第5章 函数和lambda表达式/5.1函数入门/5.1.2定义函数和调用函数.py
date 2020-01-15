96728b9174e4e29d3b880ada01b740873cc4639e
yangshuaifeng1987 <490055850@qq.com>
2020-01-15 23:17:55 +0800

    test

---
+++ 疯狂python讲义/第5章 函数和lambda表达式/5.1函数入门/5.1.2定义函数和调用函数.py
@@ -0,0 +1,21 @@
+'''
+def 函数名(形参列表):
+    执行语句
+'''
+
+
+def may_max(x, y):
+    z = x if x > y else y
+    return z
+
+
+def sai_hello(name):
+    print("==正在执行say_hello()函数")
+    return name + "你好！"
+
+
+a = 8
+b = 9
+result = may_max(a, b)
+print("result:", result)
+print(sai_hello("老习习无耻！！！！"))
