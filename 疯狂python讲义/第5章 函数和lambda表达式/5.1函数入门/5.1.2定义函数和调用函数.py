e8ac603ed1c373fb55d1c1ae3f10e660beba97da
yangshuaifeng <490055850@qq.com>
2020-01-17 17:28:32 +0800

    test

--- 疯狂python讲义/第5章 函数和lambda表达式/5.1函数入门/5.1.2定义函数和调用函数.py
+++ 疯狂python讲义/第5章 函数和lambda表达式/5.1函数入门/5.1.2定义函数和调用函数.py
@@ -100,3 +100,86 @@ def printTriangle(char, height=5):
 printTriangle('@', 6)
 printTriangle('#', height=7)
 printTriangle(char='*')
+
+print("+++++++++参数收集++++++++++")
+
+
+# 定义了支持参数收集的函数
+def test(a, *books):
+    print("books:", books)
+    for b in books:
+        print("b:", b)
+    print("a:", a)
+
+
+test(5, '疯狂Python讲义', '疯狂Android讲义')
+
+
+# 可变的形参可以处于形参列表的任意位置
+def test1(*books, a):
+    print("books:", books)
+    for b in books:
+        print("b:", b)
+    print("a:", a)
+
+
+test('疯狂Python学习', '疯狂Java学习', '回家了', 190)
+
+
+# Python收集关键字参数收集字典
+def test2(x, y, z=3, *books, **scores):
+    print("x,y,z:", x, y, z)
+    print("books:", books)
+    print("scores:", scores)
+    for k, v in scores.items():
+        print("k,v", k, v)
+
+
+test2(1, 2, '疯狂Python讲义', '疯狂Java讲义', 语文=90, 数学=89, 英语=97)
+
+print("++++++++++逆向参数收集+++++++++++")
+
+
+def test3(name, message):
+    print("name:", name)
+    print("message:", message)
+
+
+# 逆向收集参数需要在传入的列表、元组参数之前添加一个星号,在字典参数之前添加两个星号
+my_list = ['孙悟空', '欢迎来到疯狂Python学习']
+test3(*my_list)
+
+
+def test4(name, *nums):
+    print("name:", name)
+    print("nums参数:", nums)
+
+
+my_tuple = (1, 2, 3)
+test4('fkit', *my_tuple)
+
+
+# 字典也支持逆向收集
+def bar(book, price, desc):
+    print("book:", book)
+    print("price:", price)
+    print("desc:", desc)
+
+
+my_dict = {'book': '疯狂Python讲义', 'price': 90, 'desc': '这是一本系统全面的Python学习教程'}
+
+bar(**my_dict)
+
+print("+++++++++++函数的传递机制+++++++++")
+
+
+def swap(a, b):
+    # 交换a,b的值
+    a, b = b, a
+    print("swap函数里,a的值是:", a, ": b的值是：", b)
+
+
+a = 6
+b = 9
+swap(a, b)
+print('交换结束后,a的值是:', a, ":b的值是：", b)
