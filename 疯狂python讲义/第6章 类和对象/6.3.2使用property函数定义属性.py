5576802e0af491a15b0ade66206904f9ca337f9c
yangshuaifeng <490055850@qq.com>
2020-01-20 18:25:37 +0800

    test

--- 疯狂python讲义/第6章 类和对象/6.3.2使用property函数定义属性.py
+++ 疯狂python讲义/第6章 类和对象/6.3.2使用property函数定义属性.py
@@ -32,3 +32,21 @@ print(rect.height)
 del rect.size
 print(rect.height)
 print(rect.width)
+
+class User:
+    def __init__(self,first,last):
+        self.first=first
+        self.last=last
+    def getfullname(self):
+        return self.first +','+self.last
+    def setfullname(self,fullname):
+        first_last=fullname.rsplit(',')
+        self.first=first_last[0]
+        self.last=first_last[1]
+
+    fullname=property(getfullname,setfullname)
+u=User('悟空','孙')
+print(u.fullname)
+u.fullname='八戒,朱'
+print(u.fullname)
+del u.fullname
